7b1e6fcc75bc54f4b2870d020c85376de186e2eb
Erlend S <erl1sharpy@gmail.com>
2017-02-01 19:35:19 +0100

    Initial commit

---
+++ src/personform/PersonController.java
@@ -0,0 +1,40 @@
+package personform;
+
+/**
+ * Created by Erlend on 01.02.2017.
+ */
+public class PersonController {
+
+    private Person model;
+
+    @FXML TextField nameField;
+    @FXML Label nameFieldRule;
+    @FXML TextField emailField;
+
+    public void initialize() {
+        setModel(new Person("Hallvard Trætteberg", "hal@ntnu.no"));
+
+    }
+
+    public void updateView() {
+        updateNameView();
+        updateEmailView();
+    }
+
+    public void updateNameView() {
+        nameField.setText(model.getName();
+    }
+
+    public void updateNameModel() {
+        model.setName(nameField.getText());
+    }
+
+    public void updateEmailView() {
+        emailField.setText(model.getEmail());
+    }
+
+    public void updateEmailModel() {
+        model.setEmail(emailField.getText());
+    }
+
+}
